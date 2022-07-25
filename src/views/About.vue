acd7c04b340c8eebfaa15a647c8360efe5308cd0
玖柒Cherry <105545801+97-Cherry@users.noreply.github.com>
2022-07-25 21:35:04 +0800

    Add files via upload

---
+++ src/views/About.vue
@@ -0,0 +1,30 @@
+<template>
+  <div ref="echarts" id="myDiv"></div>
+</template>
+
+<script>
+import * as mycharts from 'echarts'
+export default {
+  mounted() {
+    let myEcharts=mycharts.init(this.$ref.echarts);
+    myEcharts.setOption({
+      title:{
+        text:'主标题',
+        textStyle:{
+          color:'#333'
+        }
+      }
+    })
+  },
+
+}
+</script>
+
+<style>
+#myDiv{
+  width: 500px;
+  height: 500px;
+  border: 1px solid red;
+}
+
+</style>
